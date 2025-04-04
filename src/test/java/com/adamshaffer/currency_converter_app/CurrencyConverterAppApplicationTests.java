91fc53dfaeafcf5f09623b88d751ceae84762c01
GreatDeveloper66 <ashaffe@gmail.com>
2025-04-04 14:28:23 -0500

    First Commit

---
+++ src/test/java/com/adamshaffer/currency_converter_app/CurrencyConverterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamshaffer.currency_converter_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
