245a2051359976611a61183eabe24b0492acd47a
GreatDeveloper66 <ashaffe@gmail.com>
2025-04-15 17:46:40 -0500

    Update Login Controller for inclusion of template

--- src/main/java/com/adamshaffer/currency_converter_app/Controller/LoginController.java
+++ src/main/java/com/adamshaffer/currency_converter_app/Controller/LoginController.java
@@ -1,11 +1,16 @@
 package com.adamshaffer.currency_converter_app.Controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("title", "Login Page");
+        model.addAttribute("content", "login :: content");
+        model.addAttribute("message", "Please enter your credentials to log in.");
+        model.addAttribute("error", false);
         return "login"; // This should match the name of your HTML template file (login.html)
     }
     // This class will handle the login logic for the application.
